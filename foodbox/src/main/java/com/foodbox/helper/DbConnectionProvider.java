3257f3807bf1b5e4b3d4506682d25fea7e56b48c
Suraj Singh <surajsingh9090@gmail.com>
2022-07-18 01:46:14 +0530

    DB Connection and Entities Class Creation is DONE

---
+++ foodbox/src/main/java/com/foodbox/helper/DbConnectionProvider.java
@@ -0,0 +1,23 @@
+package com.foodbox.helper;
+
+import java.sql.*;
+
+public class DbConnectionProvider {
+
+	public static Connection getCon() {
+		
+		try{  
+				Class.forName("com.mysql.cj.jdbc.Driver");  
+				Connection con=DriverManager.getConnection(  
+						"jdbc:mysql://localhost:3306/phase6-foodbox","root","root");
+				return con;
+			}
+				catch(Exception e){ 
+						System.out.println(e);
+						return null;
+				}
+		
+		}  	
+	
+}
+
